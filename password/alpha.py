2e5b86ebf952c3f25052cd2cf8eb6d4c1b8a965b
Amjed Saleel K <amjedsaleel@gmail.com>
2020-07-19 17:08:06 +0530

    new module created and it imported to views

---
+++ password/alpha.py
@@ -0,0 +1,23 @@
+import string
+from random import randint
+import random
+
+
+upper_alphabet = string.ascii_uppercase
+lower_alphabet = string.ascii_lowercase
+special_character = ['!', '@', '$', '&', '*', ':', ';', '.', '?']
+
+pass_list = []
+
+
+def generate(password_length):
+    for i in range(password_length):
+        comp_1 = random.choice(lower_alphabet)
+        comp_2 = random.choice(upper_alphabet)
+        comp_3 = randint(0, 9)
+        comp_4 = random.choice(special_character)
+        value = str(random.choice([comp_1, comp_2, comp_3, comp_4]))
+        # print(value, end='')
+        pass_list.append(value)
+    password = ''.join([str(elem) for elem in pass_list])
+    return password
--- password/views.py
+++ password/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from . import alpha
 
 # Create your views here.
 
 
 def index(request):
+    if request.method == "POST":
+        password_length = request.POST.get('password-length')
+        print(password_length)
+
+        gen = alpha.generate(int(password_length))
+        print(gen)
+
     context = {}
     return render(request, 'index.html', context)
 
